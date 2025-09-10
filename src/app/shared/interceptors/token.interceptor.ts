56062556f53e8ee08fe0c54d98386fb2e1f949f4
Ali S.Darwish <ali.sdarwish97@gmail.com>
2025-09-10 16:54:18 +0300

    refactor: improve token interceptor logic and update environment base URL

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -3,12 +3,13 @@ import { StorageConstant } from '../config/storage.constant';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const localStorageData = localStorage.getItem(StorageConstant.AUTH_USER)
-    ? JSON.parse(localStorage.getItem(StorageConstant.AUTH_USER) || '')
+    ? JSON.parse(localStorage.getItem(StorageConstant.AUTH_USER) || '{}')
     : null;
+
+  let headers = req.headers;
 
   if (localStorageData) {
     const { token, language } = localStorageData;
-    let headers = req.headers;
 
     if (token) {
       headers = headers.set('Authorization', `Bearer ${token}`);
@@ -17,9 +18,9 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     if (language) {
       headers = headers.set('Accept-Language', language);
     }
-
-    req = req.clone({ headers });
   }
+
+  req = req.clone({ headers, withCredentials: true });
 
   return next(req);
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  base_url: 'http://13.60.179.21/',
+  // base_url: 'http://13.60.179.21/',
+  base_url: 'https://pos-be-production-2f3f.up.railway.app/',
   //   base_url: 'http://localhost:3000/',
   api_prefix: 'api/',
   api_version: 'v0.0.0',
